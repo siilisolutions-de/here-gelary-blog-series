8ad248ddbe9a8f6d8226de546a569f8ffe61a535
Florian Plank <polarblau@gmail.com>
2016-10-11 10:52:09 +0200

    Post 4: Advanced Routing -- Route selection and directions.

--- 4-advanced-routing/scripts/map.js
+++ 4-advanced-routing/scripts/map.js
@@ -25,7 +25,7 @@ HEREMap.prototype.updateMyPosition = function(event) {
 
   // Remove old location marker if it exists
   if (this.myLocationMarker) {
-    this.myLocationMarker.remove();
+    this.removeMarker(this.myLocationMarker);
   }
 
   // Draw the route from current location to HERE HQ if not yet drawn
@@ -33,25 +33,68 @@ HEREMap.prototype.updateMyPosition = function(event) {
     this.drawRoute(this.position, HEREHQcoordinates);
   }
 
-  this.myLocationMarker = this.addMarker(this.position);
+  this.myLocationMarker = this.addMarker(this.position, 'iceCream');
   this.map.setCenter(this.position);
 };
 
-HEREMap.prototype.addMarker = function(coordinates) {
-  var marker = new H.map.Marker(coordinates);
+HEREMap.prototype.addMarker = function(coordinates, icon) {
+  var markerOptions = {};
+
+  // Dictonary for icon data
+  var icons = {
+    iceCream: {
+      url: './images/marker-gelato.svg',
+      options: {
+        size: new H.math.Size(26, 34),
+        anchor: new H.math.Point(14, 34)
+      }
+    },
+    origin: {
+      url: './images/origin.svg',
+      options: {
+        size: new H.math.Size(30, 36),
+        anchor: new H.math.Point(12, 36)
+      }
+    },
+    destination: {
+      url: './images/destination.svg',
+      options: {
+        size: new H.math.Size(30, 36),
+        anchor: new H.math.Point(12, 36)
+      }
+    }
+  };
+
+  if (icons[icon]) {
+    markerOptions = {
+      icon: new H.map.Icon(icons[icon].url, icons[icon].options)
+    };
+  }
+
+  var marker = new H.map.Marker(coordinates, markerOptions);
   this.map.addObject(marker);
+
+  return marker;
+};
+
+HEREMap.prototype.removeMarker = function(marker) {
+  this.map.removeObject(marker);
 };
 
 HEREMap.prototype.drawRoute = function(fromCoordinates, toCoordinates) {
   var routeOptions = {
     mode: 'fastest;car',
     representation: 'display',
-    alternatives: 3,
+    alternatives: 2,
+    routeattributes: 'waypoints,summary,shape,legs',
     waypoint0: Utils.locationToWaypointString(fromCoordinates),
     waypoint1: Utils.locationToWaypointString(toCoordinates)
   };
 
-  this.routes = new HERERoute(this.map, this.platform, routeOptions);
+  this.addMarker(fromCoordinates, 'origin');
+  this.addMarker(toCoordinates, 'destination');
+
+  this.route = new HERERoute(this.map, this.platform, routeOptions);
 };
 
 HEREMap.prototype.resizeToFit = function() {
--- 4-advanced-routing/scripts/route.js
+++ 4-advanced-routing/scripts/route.js
@@ -1,14 +1,32 @@
 function HERERoute (map, platform, routeOptions) {
 
   var router = platform.getRoutingService();
+  var routeLineStyles = {
+    normal: { strokeColor: 'rgba(0, 85, 170, 0.5)', lineWidth: 3 },
+    selected: { strokeColor: 'rgba(255, 0, 0, 0.7)', lineWidth: 7 }
+  };
+  var selectedRoute;
 
   var onSuccess = function(result) {
     if (result.response.route) {
-      var routes = result.response.route;
-      var routeLines = routes.map(drawRoute);
-      var routeLineGroup = new H.map.Group({ objects: routeLines });
+      var routeLineGroup = new H.map.Group();
+
+      var routes = result.response.route.map(function(route) {
+        var routeLine = drawRoute(route);
+        routeLineGroup.addObject(routeLine);
+
+        return {
+          route: route,
+          routeLine: routeLine
+        };
+      });
+
       map.addObject(routeLineGroup);
       map.setViewBounds(routeLineGroup.getBounds());
+
+      this.routePanel = new HERERoutesPanel(routes,
+        { onRouteSelection: onRouteSelection }
+      );
     }
   };
 
@@ -16,8 +34,17 @@ function HERERoute (map, platform, routeOptions) {
     console.error('Oh no! There was some communication error!', error);
   };
 
+  var onRouteSelection = function(route) {
+    console.log('A route has been selected.', route);
+    if (selectedRoute) {
+      selectedRoute.routeLine.setStyle(routeLineStyles.normal).setZIndex(1);
+    }
+
+    route.routeLine.setStyle(routeLineStyles.selected).setZIndex(10);
+    selectedRoute = route;
+  };
+
   var drawRoute = function(route) {
-    // Pick the route's shape:
     var routeShape = route.shape;
 
     // Create a strip to use as a point source for the route line
@@ -30,9 +57,9 @@ function HERERoute (map, platform, routeOptions) {
     });
 
     // Create a polyline to display the route:
-    var routeLine = new H.map.Polyline(strip, {
-      style: { strokeColor: 'rgba(0, 85, 170, 0.4)', lineWidth: 3 }
-    });
+    var routeLine = new H.map.Polyline(strip,
+      { style: routeLineStyles.normal }
+    );
 
     return routeLine;
   };
